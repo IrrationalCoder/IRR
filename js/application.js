53d4540d785bf911c0e7a9fa11bdc72c0142d452
IrrationalCoder <irrationalcoder@gmail.com>
2015-05-21 00:39:36 +0800

    preloaded changeToGrey function
    This function will change each part of the border color to grey every 3
    seconds (Clockwise).

--- js/application.js
+++ js/application.js
@@ -1,5 +1,26 @@
 // JavaScript Document
 
+var changeToGrey =  function(){
+	var counter = 0
+	var changeColor = setInterval(function(){
+	   switch (counter){
+			case 0:
+				$('.animate-logo').css({'border-top': '20px solid grey'})	
+				break;
+			case 1:
+				$('.animate-logo').css({'border-right': '20px solid grey'});
+				break;
+			case 2:
+				$('.animate-logo').css({'border-bottom': '20px solid grey'});
+				break;
+			case 3:
+				$('.animate-logo').css({'border-left': '20px solid grey'});	
+		}
+		
+    counter++;
+}, 3000);
+}
+
 var preLoader = function(){
 	
 	$('body').prepend('<div class="preloader"><div class="animation-wrapper"><div class="animate-logo"></div></div></div>');
